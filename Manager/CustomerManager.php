0e405b48d1061aefe41e08b1eec8b9b867532780
Javi H. Gil <javihgil@gmail.com>
2020-05-05 10:13:37 +0200

    Cleanup CustomerManager

--- Manager/CustomerManager.php
+++ Manager/CustomerManager.php
@@ -2,49 +2,9 @@
 
 namespace Softspring\CustomerBundle\Manager;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Softspring\CrudlBundle\Manager\CrudlEntityManagerTrait;
-use Softspring\CustomerBundle\Model\CustomerInterface;
-use Softspring\CustomerBundle\Platform\ApiManagerInterface;
+use Softspring\CrudlBundle\Manager\DefaultCrudlEntityManager;
 
-class CustomerManager implements CustomerManagerInterface
+class CustomerManager extends DefaultCrudlEntityManager implements SourceManagerInterface
 {
-    use CrudlEntityManagerTrait;
 
-    /**
-     * @var EntityManagerInterface
-     */
-    protected $em;
-
-    /**
-     * @var ApiManagerInterface
-     */
-    protected $api;
-
-    /**
-     * CustomerManager constructor.
-     * @param EntityManagerInterface $em
-     * @param ApiManagerInterface $api
-     */
-    public function __construct(EntityManagerInterface $em, ApiManagerInterface $api)
-    {
-        $this->em = $em;
-        $this->api = $api;
-    }
-
-    public function getTargetClass(): string
-    {
-        return CustomerInterface::class;
-    }
-
-    public function createEntity()
-    {
-        $class = $this->getEntityClassReflection()->name;
-
-        /** @var CustomerInterface $entity */
-        $entity = new $class;
-        $entity->setPlatform($this->api->platformId());
-
-        return $entity;
-    }
 }
